79d9eb16e46b70d667ee4ab79046a91da653a49a
NajibIbrahim <najibibra11@gmail.com>
2019-07-25 12:57:22 +0700

    Flappy Copter

---
+++ Assets/Scripts/GeneratorObs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorObs : MonoBehaviour {
+int score = 0;
+public GameObject rocks;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1f, 1.5f);
+	}
+	void CreateObstacle(){
+		Instantiate (rocks);
+		score++;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnGUI(){
+		GUI.color=Color.black;
+		GUILayout.Label("Score: "+score.ToString());
+	}
+}
---
+++ Assets/Scripts/ObstacieinAct.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacieinAct : MonoBehaviour {
+
+public Vector2 veloc = new Vector2(-4,0);
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = veloc;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
